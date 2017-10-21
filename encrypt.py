b2fdf373f967580f25526732b4bfcd564c2f7ff0
Rigel Di Scala <zedr@zedr.com>
2017-10-21 10:43:50 +0100

    Implement cypher and utils

--- encrypt.py
+++ encrypt.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-P = 10267
-Q = 40163
-E = 69439
+import base64
+
+# Cannot pick primes greater than 1000
+P = 389
+Q = 673
+E = 31139
 
 
 def is_prime(num: int) -> bool:
@@ -17,9 +20,46 @@ def egcd(a: int, b: int) -> tuple:
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
 
+
 def modinv(a: int, m: int) -> int:
     g, x, y = egcd(a, m)
     if g != 1:
         raise Exception('modular inverse does not exist')
     else:
         return x % m
+
+
+def _enc(num: int, key: tuple) -> int:
+    modulus, a = key
+    return pow(num, a, modulus) # (num ** a) % modulus
+
+
+def _enc_str(text: str, key: tuple) -> bytes:
+    return "".join(chr(_enc(ord(ch), key)) for ch in text)
+
+
+def encrypt(plaintext: str, key: tuple) -> str:
+    return base64.b64encode(_enc_str(plaintext, key).encode())
+
+
+def decrypt(cyphertext: bytes, key: tuple) -> str:
+    return _enc_str(base64.b64decode(cyphertext).decode(), key)
+
+
+assert is_prime(P)
+assert is_prime(Q)
+assert is_prime(E)
+
+MODULUS = P * Q
+T = (P - 1) * (Q - 1)
+# UNSAFE: skip the check for coprimes
+PRIVATE_KEY = (MODULUS, modinv(E, T))
+PUBLIC_KEY = (MODULUS, E)
+PUBLIC_KEY_NAME = "{}-{}".format(*PUBLIC_KEY)
+
+if __name__ == "__main__":
+    plaintext = "dublin"
+    cyphertext = encrypt(plaintext, PRIVATE_KEY)
+    print(cyphertext)
+    plaintext2 = decrypt(cyphertext, PUBLIC_KEY)
+    print(plaintext2)
